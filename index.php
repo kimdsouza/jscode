482f904dccbd0c49c9c508696fd905238e4c992f
Kimberly <kimdsouza.work@gmail.com>
2017-12-06 15:25:00 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<script type="text/javascript">
+var i=0;
+for(i=0;i<=100;i++){
+if(i%3==0){
+document.write('Earth');
+  }
+if(i%5==0){
+document.write('Jupiter');
+  }
+if(i%9==0){
+document.write('Pluto');
+  }
+
+}
+</script>
